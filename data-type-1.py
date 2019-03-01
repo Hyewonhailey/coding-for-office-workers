7e2bdaea0c2184db77ac3d8cd4ea7c2611e3ffd0
이혜원 <H@Hui-MacBook-Pro.local>
2019-03-01 14:05:15 +0900

    add data-type-1.py int, float, str

---
+++ data-type-1.py
@@ -0,0 +1,19 @@
+
+
+print(1 + 1)
+# float
+
+print(1.5 + 2.1)
+# str - string
+
+print("Hello")
+
+print("hello" + "1")
+
+print(int(4 / 2))
+
+print(float(4/2))
+
+print(int("1"))
+print("1")
+print(int("1.5"))
