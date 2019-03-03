aa09c9537d7dee9a6cdbf50d4652d3af8bd86e70
이혜원 <H@Hui-MacBook-Pro.local>
2019-03-03 16:55:28 +0900

    05

---
+++ 05-for-loop.py
@@ -0,0 +1,18 @@
+# for, range
+# while
+
+# for: list, tuple 등의 목록에 사용
+# for num in range(1, 10):
+#     print(num)
+#
+# num_list = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+# for num in num_list:
+    # print(num)
+
+#  while (if + for) + 조건으로 사용
+# while True: # 무한루프
+
+a = 1
+while a < 10:
+    print(a)
+    a = a + 1
